d6612f17ac1a00f0ab32154171d2e81325cde1c8
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-11-11 15:41:34 +0100

    Make shell.nix more reproducible

--- shell.nix
+++ shell.nix
@@ -1,5 +1,23 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation {
+let
+  pkgs = import <nixpkgs> {};
+  eldev = pkgs.stdenv.mkDerivation rec {
+    pname = "eldev";
+    version = "0.9.3";
+    src = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/doublep/eldev/${version}/bin/eldev";
+      sha256 = "0ikhhfxm1rz3wp37spsy8bcnx5071ard71pd1riw09rsybilxhgn";
+    };
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    unpackPhase = "true";
+    installPhase = ''
+      install -D -v -m555 "$src" "$out/bin/eldev"
+      wrapProgram "$out/bin/eldev" --set ELDEV_EMACS "${pkgs.emacs}/bin/emacs"
+    '';
+  };
+in pkgs.mkShell {
   name = "skempo-dev-env";
   buildInputs = [ eldev ];
+  shellHook = ''
+    export ELDEV_DIR=$PWD/.eldev
+  '';
 }
